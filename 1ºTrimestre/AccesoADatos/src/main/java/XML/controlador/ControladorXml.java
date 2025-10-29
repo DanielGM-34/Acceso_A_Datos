3d6bd21a8418cde134bf0905f93b75e3e0ee5026
https://github.com/DanielGM-34/2DAM_Programacion <dgarmen630@g.educaand.es>
2025-10-29 13:31:50 +0100

    xml

---
+++ 1ºTrimestre/AccesoADatos/src/main/java/XML/controlador/ControladorXml.java
@@ -0,0 +1,25 @@
+package XML.controlador;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import XML.DomEmpleado;
+import modelo.Empleado; 
+
+public class ControladorXml{
+	private static final Logger logger = LogManager.getLogger(DomEmpleado.class);
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DomEmpleado xmlEmpl = new DomEmpleado();
+		try {
+			Empleado e = xmlEmpl.leerEmpleadoDesdeXML("empleado.xml");
+			logger.info(e.toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
